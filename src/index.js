d719f29d3ff2397455762f5fd7f2a7e8cc0b4d48
afrin786akhtar <afrin786akhtar@gmail.com>
2022-09-05 16:04:46 +0530

    Blog-Site

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://Shreyad:ULCn2m1OMNHF7ME0@cluster0.ufn2trt.mongodb.net/blog-site-project", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
---
+++ src/model/authorModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const emailValidator = require('email-validator')
+
+const authorSchema = new mongoose.Schema({
+     fname:{
+        type : String ,
+        required : true
+     },
+      lname: {
+        type : String ,
+        required : true
+      }, 
+      title: {
+        type : String ,
+        required : true,
+        enum : [Mr, Mrs, Miss]
+      }, 
+      email: {
+        type : emailValidator.validate(),
+        required : true , 
+        unique : true
+      }, 
+        password: {
+            type : String,
+            required : true
+        }
+
+} , {timestamps: true} )
+
+module.exports = mogoose.model('author' , authorSchema)
+
+
+
+
+
+// { fname: { mandatory}, lname: {mandatory}, title: {mandatory, enum[Mr, Mrs, Miss]}, email: {mandatory, valid email, unique}, password: {mandatory} }
